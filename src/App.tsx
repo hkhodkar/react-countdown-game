fc1a8685b01aa3d03661208ff91b3a45ae7e973c
hatef khodkar <hatef.khodkar@hotmail.com>
2024-03-07 13:06:42 +0400

    react count down game

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './App.css'
+import Player from './components/Player.jsx';
+function App() {
+
+  return (
+    <Fragment>
+      <Player />
+      <div id="challenges"></div>
+    </Fragment>
+  )
+}
+
+export default App
