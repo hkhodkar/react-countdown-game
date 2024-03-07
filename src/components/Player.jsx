8657f406289a3fd93fc99edd1c56fef2096d2c58
hatef khodkar <hatef.khodkar@hotmail.com>
2024-03-07 14:09:34 +0400

    feat(input): implement player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,27 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
-    return (
-      <section id="player">
-        <h2>Welcome unknown entity</h2>
-        <p>
-          <input type="text" />
-          <button>Set Name</button>
-        </p>
-      </section>
-    );
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const playerNameRef = useRef();
+
+  const setNameHandler = () => {
+    if (playerNameRef.current.value.trim().length === 0) {
+      setEnteredPlayerName(null);
+    } else {
+      setEnteredPlayerName(playerNameRef.current.value);
+      playerNameRef.current.value = '';
+    }
   }
-  
+
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input ref={playerNameRef} type="text" />
+        <button onClick={setNameHandler}>Set Name</button>
+      </p>
+    </section>
+  );
+}
